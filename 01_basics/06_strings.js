3f60cda8c00cfbc2dd0964d6ad578f87fd761d92
dhrubb <dhrubbgupta@gmail.com>
2025-04-04 09:16:44 +0530

    Learnt about string function like replace,includes, split,slice ,charAt,indexOf,tolowerCase , to UpperCase refer MDN doc as well

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -21,4 +21,36 @@ console.log(`Hello my name is ${name}  and my repo count is ${repoCount}`)
 //  Better Readability – Reduces string concatenation (+ operator). 
 
 //there's one better way to declare variables
-const gameName = new String('Hiteshhc')
+const gameName = new String('Hitesh-hc')
+
+console.log(gameName) // String { 'Hiteshhc' }
+console.log(typeof gameName) // object
+console.log(gameName.length) // 8
+console.log(gameName[0]) // H
+console.log(gameName.__proto__) // H
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase()) // HITESHHC
+console.log(gameName.toLowerCase()) // hiteshhc
+//original value remains the same
+console.log(gameName.charAt(2)) // t
+console.log(gameName.indexOf('t')) // 2
+
+const newString = gameName.substring(0,4)
+console.log(newString) // Hite
+// console.log(gameName.slice(0,4)) // Hite
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString) // Hite
+
+const newStringOne = "  Hitesh   "
+console.log(newStringOne)
+console.log(newStringOne.trim()) // Hitesh
+console.log(newStringOne.trimStart()) // Hitesh
+console.log(newStringOne.trimEnd()) // Hitesh
+
+const url = "https://www.hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20','-')) // https://www.hitesh.com/hitesh-choudhary
+console.log(url.includes('sundar'))
+// console.log(spli)
