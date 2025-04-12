e65d89dae7a4ecfdedae2cf5c885cbdfc538e373
dhrubb <dhrubbgupta@gmail.com>
2025-04-12 13:26:51 +0530

    javacript arrays in (02_basics)

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,18 @@
+//array
+
+const myArr = [0,1,2,3,4,5]
+const myHeroes = ["spiderman", "ironman", "thor", "hulk"]
+
+const myArr2 = new Array(1,2,3,4)
+
+
+//javascript arrays are resizable
+
+console.log(myArr[1])
+
+
+//Array methods
+
+
+myArr.push(6) //adds an element at the end of the array
+console.log(myArr)
