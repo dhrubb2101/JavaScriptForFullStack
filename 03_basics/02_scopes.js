dd5aba468e7bf2ed63ba21b9ec39f620c012926a
dhrubb <dhrubbgupta@gmail.com>
2025-04-16 11:52:07 +0530

    scope for if-else loop and how to use and declare function and if we store it in a variable we can't put the when we call the function above the function , without declaring the function  inside the variable we can but not when we store it in a variable

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,11 +8,63 @@ var c = 30
 if (true) {
     let a = 100
     const b = 200
-    console.log("INNER", a) // 100
+    // console.log("INNER", a) // 100
     c=300
    
 }
-console.log(a) // 10
-console.log(c) // 300 // that's why we never use VAR becuase what value we defined in the block just overrode the what we defined in the global scope
+// console.log(a) // 10
+// console.log(c) // 300 // that's why we never use VAR becuase what value we defined in the block just overrode the what we defined in the global scope
 
 //never used VAR ever 
+
+// ===================================
+
+function one(){
+    const username = "Hitesh"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username) // Hitesh
+    }
+    // console.log(website)
+
+    two()
+    
+}
+
+one()
+
+//in nested function we can access the outer function variable but not the other way around
+//that is child function can access the parent function variable but not the other way around
+
+// =====================================
+
+// if-else scope
+
+if(true){
+    const username = "Hitesh"
+    if(username === "Hitesh"){
+        const website = "youtube"
+        // console.log(username + " " + website) // Hitesh
+    }
+    // console.log(website) // ReferenceError: website is not defined
+}
+// console.log(username) // ReferenceError: username is not defined
+// in both cases we are not able to access the variable outside the block scope
+
+// =====================================interesting==================
+
+function addone(num){ //function
+    return num + 1
+
+}
+
+addone(5)
+
+//another way
+
+const addTwo = function(num){ //function expression where function is stored in a variable
+    return num + 2
+}
+
+addTwo(5)
