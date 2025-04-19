231b41844d1217d70dd8350ce3989ce0b498fc93
dhrubb <dhrubbgupta@gmail.com>
2025-04-19 13:17:18 +0530

    while loop and do_while loop how it works and how array works in a while loop

--- 05_iterations/01_forloop.js
+++ 05_iterations/01_forloop.js
@@ -57,7 +57,7 @@ for (let i = 0; i < 10; i++) {
 for (let index = 1; index <= 20; index++) {
     if(index==5){
         console.log(`Detected 5`)
-        continue;
+        continue ;
     }
     console.log(`Value of i is ${index}`)
 }
